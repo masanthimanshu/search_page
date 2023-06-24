6405598c7b0075ac78d186faa257c15d5b9038e7
Himanshu Masant <himanshumasant@uxdlabtech.com>
2023-06-24 10:51:11 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("input_form");
+const query = document.getElementById("search_query");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = query.value;
+
+  window.open(`https://www.google.com/search?q=${text}`);
+});
