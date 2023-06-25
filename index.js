5d728144aedb97b952a0491dd02e61c033316697
Himanshu Masant <masanthimanshu@gmail.com>
2023-06-25 08:50:51 +0530

    Updates

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const query = document.getElementById("search_query");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const text = query.value;
+  query.value = "";
 
   window.open(`https://www.google.com/search?q=${text}`);
 });
